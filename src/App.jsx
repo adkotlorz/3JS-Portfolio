44ec69b8cd109d8bb05431542708b3b19baa0ff2
adkotlorz <adkotlorz@gmail.com>
2023-11-28 19:29:44 +0100

    assets and folder structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+import { Navbar } from "./components";
+
+import {
+  Home,
+  About,
+  Projects,
+  Contact,
+} from "./pages";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+
+import Island from "../models/island";
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        Popup
+      </div> */}
+
+      <Canvas
+        camera={{ near: 0.1, far: 1000 }}
+        className="w-full h-screen bg-transparent"
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
