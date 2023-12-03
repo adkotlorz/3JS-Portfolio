5886ec287f54647ff9b90ef4acbc58a7aa529285
adkotlorz <adkotlorz@gmail.com>
2023-12-03 23:07:51 +0100

    animations done with some styling changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,18 @@ const Navbar = () => {
   return (
     <header className="header">
       <NavLink to="/">
-        <img
-          src={logo}
-          alt="logo"
-          className="w-18 h-18 object-contain"
-        />
+        <img src={logo} alt="logo" className="w-18 h-18 object-contain" />
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
           to="/about"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-600"
-              : "text-black"
-          }
+          className={({ isActive }) => (isActive ? "text-blue-600" : "text-black")}
         >
           About
         </NavLink>
         <NavLink
           to="/projects"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-600"
-              : "text-black"
-          }
+          className={({ isActive }) => (isActive ? "text-blue-600" : "text-black")}
         >
           Projects
         </NavLink>
