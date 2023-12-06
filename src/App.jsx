3635604ed5c2a36aacf16f90c3da58e2a30f999e
adkotlorz <adkotlorz@gmail.com>
2023-12-06 20:09:00 +0100

    small tweaks with constants and project/experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import { Navbar } from "./components";
+import { Footer, Navbar } from "./components";
 
-import {
-  Home,
-  About,
-  Projects,
-  Contact,
-} from "./pages";
+import { Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
@@ -21,16 +12,17 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route
-            path="/about"
-            element={<About />}
-          />
-          <Route
-            path="/projects"
-            element={<Projects />}
-          />
-          <Route
-            path="/contact"
-            element={<Contact />}
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
           />
         </Routes>
       </Router>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Navbar from "./Navbar";
 import Loader from "./Loader";
 import HomeInfo from "./HomeInfo";
 import Alert from "./Alert";
+import CTA from "./CTA";
+import Footer from "./Footer";
 
-export { Navbar, Loader, HomeInfo, Alert };
+export { Navbar, Loader, HomeInfo, Alert, CTA, Footer };
